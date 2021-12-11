5ee495c876797ad8aba7df8cd4da7318882a5b61
victor plantevin <victor.plantevin@gmail.com>
2021-12-11 15:43:35 +0100

    exercice 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 end
